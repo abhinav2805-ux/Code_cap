667a212aabb3d0d54160f7853ae9b44edd5160fc
Abhinav Gupta <gupta380189@gmail.com>
2024-07-17 10:34:07 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
