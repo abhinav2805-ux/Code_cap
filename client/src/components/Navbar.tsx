5bbb0d69ea9c1950e2e9c27c908042a95eb132a8
Ansh Jain <anshjain9159@gmail.com>
2024-08-14 22:04:12 +0530

    chore: add favicon, fixed featured hackathons and navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,33 +1,47 @@
-import React, { useState ,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
-import {  Button } from '@mui/material';
+import { Button } from '@mui/material';
 import { useToast } from './ui/use-toast';
 import { AvatarFallback, AvatarImage, Avatar } from './ui/avatar';
 import { useNavigate } from 'react-router-dom';
+
 interface FormData {
-  
   github: string;
- 
   avatar: string;
 }
-
 
 const Navbar: React.FC = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const { toast } = useToast();
+  const navigate = useNavigate();
 
-  const {toast} = useToast();
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
   const [formData, setFormData] = useState<FormData>({
-  
     github: '',
-    
     avatar: '',
   });
+
+  useEffect(() => {
+    const username = getCookieValue('user');
+    console.log(username);
+    fetch(`http://localhost:3000/api/user/getProfile/${username}`, {
+      credentials: 'include'
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setFormData({
+          github: data[0].Github,
+          avatar: data.avatar,
+        });
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching user data:', error);
+      });
+  }, []);
+
   function getCookieValue(name: string) {
     const cookies = document.cookie.split('; ');
     for (let cookie of cookies) {
@@ -38,27 +52,10 @@ const Navbar: React.FC = () => {
     }
     return null;
   }
-  useEffect(() => {
-    const username = getCookieValue('user');
-    console.log(username);
-    fetch(`http://localhost:3000/api/user/getProfile/${username}`, {
-      credentials: 'include'
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setFormData({
-         
-          github: data[0].Github,
-         
-          avatar: data.avatar,
-        });
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching user data:', error);
-      });
-  }, []);
 
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
@@ -67,7 +64,7 @@ const Navbar: React.FC = () => {
   const closeSidebar = () => {
     setIsOpen(false);
   };
-  const navigate = useNavigate();
+
   const handleLogout = async () => {
     try {
       const response = await fetch('http://localhost:3000/api/user/signOut', {
@@ -79,14 +76,13 @@ const Navbar: React.FC = () => {
       });
   
       if (response.ok) {
-        // Show the toast notification
         toast({
           title: "LogOut Successfully",
           description: "Logout Successful",
         });
         
         setTimeout(() => {
-          navigate('/'); // Redirect to home page
+          navigate('/');
         }, 2000);
       } else {
         console.error('Failed to sign out');
@@ -110,17 +106,16 @@ const Navbar: React.FC = () => {
           <a href="/about" className="hover:text-yellow-500 text-2xl font-medium">ABOUT</a>
         </div>
 
-        <div className="flex justify-between md:justify-end items-center w-1/2 md:w-[10%] h-[50px] md:h-full">
-        <Button
-  className=" w-1/3 h-full"
-  onClick={toggleDropdown}
->
-  <Avatar className="w-full h-full rounded-full object-cover">
-    <AvatarImage src={`https://avatars.githubusercontent.com/${formData.github}`} />
-    <AvatarFallback>CN</AvatarFallback>
-  </Avatar>
-</Button>
-
+        <div className="hidden md:flex justify-end items-center w-[10%] h-full">
+          <Button
+            className="w-1/3 h-full"
+            onClick={toggleDropdown}
+          >
+            <Avatar className="w-full h-full rounded-full object-cover">
+              <AvatarImage src={`https://avatars.githubusercontent.com/${formData.github}`} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </Button>
 
           {dropdownOpen && (
             <div className="absolute right-0 mt-40 px-4 space-y-2 py-2 w-48 bg-black text-white rounded-lg shadow-lg flex-col justify-center items-center">
@@ -129,8 +124,9 @@ const Navbar: React.FC = () => {
               </div>
               <div>
                 <a href="/" className="text-black rounded-xl font-semibold block px-4 py-2 text-center text-lg bg-white"
-                  onClick={() => {
-                  handleLogout();
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleLogout();
                   }}
                 >
                   Log Out
@@ -154,13 +150,13 @@ const Navbar: React.FC = () => {
           <a href="/hackathons" className="text-white text-xl font-medium hover:text-yellow-500" onClick={closeSidebar}>HACKATHONS</a>
           <a href="/about" className="text-white text-xl font-medium hover:text-yellow-500" onClick={closeSidebar}>ABOUT</a>
           <a href="/edit-profile" className="text-white text-xl font-medium hover:text-yellow-500" onClick={closeSidebar}>EDIT PROFILE</a>
-          <a href="#" className="text-white text-xl font-medium hover:text-yellow-500" onClick={closeSidebar}>STATUS</a>
           <a
             href="/"
             className="text-white text-xl font-medium hover:text-yellow-500"
-            onClick={() => {
-              // Prevent default link behavior
-              handleLogout(); // Handle logout
+            onClick={(e) => {
+              e.preventDefault();
+              handleLogout();
+              closeSidebar();
             }}
           >
             LOG OUT
